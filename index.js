b4797ee18c40452dc9e54f489306c3b469b7d0ac
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-11-04 00:14:41 +0200

    basic js output practiced

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// 1
+
 let x1 = { b: 1, c: 2 };
 let y1 = Object.keys(x1);
-console.log(y1.length); 
+console.log(y1.length);
+
+// 2
+let x = '{ "b": 1, "c": 2 }';
+let y = JSON.parse(x);
+console.log(typeof y);
+console.log(y);
+
