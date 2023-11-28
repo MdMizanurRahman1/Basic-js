1cf5184ef6d83b4718ec423f5f5b0540f352baa4
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-11-28 12:27:42 +0200

    basic js scope

--- scope.js
+++ scope.js
@@ -53,3 +53,22 @@ function ty() {
 }
 ty();
 console.log(t);
+
+var math = true;
+var biology = true;
+var bangla = false;
+
+if (math == true) {
+    if (biology == true) {
+        if (bangla == true) {
+
+        } else {
+            console.log('Bangla baloo paro na');
+        }
+    } else {
+        console.log('bio balo paro na');
+    }
+
+} else {
+
+}
